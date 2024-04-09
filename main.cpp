ab2e730ed2f0cef1092bf9f765ef5747c396270d
totycardenasa <totycardenas102004@gmail.com>
2024-04-08 22:13:29 -0500

    ULTIMO COMMIT
    Se terminó de organizar la documentación y se pusieron algunos comentarios en el codigo

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "Desafio_1.h"
 #include <iostream>
 using namespace std;
-
+ //(4,3,1,-1,0,-100)
 
 
 int main (){
@@ -17,8 +17,8 @@ int main (){
             dato=llamado[contador]; // Se guarda cada elementos de "K" (contando el "-100")
             contador=contador+1; // el numero de elementos de K (contando el "-100")
         }
-        int Tamanio_K = contador-1;
-        int cantidad_condiciones = Tamanio_K-2;
+        int Tamanio_K = contador-1; // DIM del arreglo K, sin incluir el -100.
+        int cantidad_condiciones = Tamanio_K-2; // cantidad de condiciones del arreglo k
         if (fila1>columna1){// fila es mayor que columna?
             if (fila1%2 == 0){ // verificar si es par ó impar
                 dim_M1 = fila1+1;
@@ -57,7 +57,7 @@ int main (){
         int cantidad_de_ArrdeX=cantidad_condiciones+1;
         int arreglo_X[cantidad_de_ArrdeX];
         int arreglo_rotaciones[cantidad_de_ArrdeX];
-        int contador_posicion_x = 1;
+        int contador_posicion_x = 1;// contador para iterar
         int** segunda_matriz = crear_matrices(dim_variable);
         arreglo_X[0]=dim_M1;
         arreglo_rotaciones[0]=0;
@@ -67,7 +67,7 @@ int main (){
                 int rotaciones=0;
                 while(rotaciones<=3){
                     llamado_FUNcomprobador =  comprobador(primer_matriz, segunda_matriz, fila1-1, columna1-1, fila2-1, columna2-1,llamado[condiciones]);
-                    if(llamado_FUNcomprobador == false){
+                    if(llamado_FUNcomprobador == false){ // toca rotar
                         rotar_noventa(segunda_matriz, dim_variable);
                         rotaciones=rotaciones+1;
                     }
@@ -85,22 +85,16 @@ int main (){
                         columna1 = columna2;
                         fila2= llamado[0];
                         columna2 = llamado[1];
-                        rotaciones = 10;
+                        rotaciones = 10;// para que se salga del while sin entrar al if
                     }
                     if(rotaciones==4){
                         liberar_mem_neutra(segunda_matriz, dim_variable);
                         dim_variable=dim_variable+2;
                         segunda_matriz = crear_matrices(dim_variable);
-                        if(dim_variable1 < dim_variable){
-                            fila1 = fila2;
-                            columna1 = columna2;
                             fila2=fila2+1;
                             columna2=columna2+1;
-                        }
-                        else{
-                            fila2=fila2+1;
-                            columna2=columna2+1;
-                        }
+
+
                     }
                 }
             }
